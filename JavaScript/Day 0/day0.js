162a9a2c1f3822a3ff6a4346e4af85d3af0ffe55
Oswaldo Yanez <oswaldo.business31@gmail.com>
2022-08-05 10:34:59 -0400

    Test Day.

---
+++ JavaScript/Day 0/day0.js
@@ -0,0 +1,5 @@
+const greetings = (name) => {
+    console.log(`Hello I AM ${name}, I'm ready for this new challenge.`)
+}
+
+greetings("BATMAN. 🦇")
---
+++ Python/Day 0/day0.py
@@ -0,0 +1,17 @@
+def greetings(name):
+    print(f"Hello my name is {name}, I'm ready for this new challenge.")
+
+
+def run():
+    name = input("Enter your name: ") 
+    greetings(name)
+
+
+
+if __name__ == "__main__":
+    run()
+
+
+
+
+
